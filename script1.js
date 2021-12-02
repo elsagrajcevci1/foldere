74bd6006fc9291691ff9649110d84856f8e44a9b
elsagrajcevci1 <elsagrajcevci@gmail.com>
2021-12-02 12:39:35 +0100

    first commit

---
+++ script1.js
@@ -0,0 +1,55 @@
+let username = 'Elsa';
+let birthYear = 2005;
+let isKid = false;
+let isAdult = true;
+let height = 1.73;
+let temerature = 20;
+let quotes = 'Life is awesome';
+
+let user_email;
+let score = null; 
+
+
+console.log (' Hello world!');
+console.log (quotes);
+console.log ('quotes');
+ quotes = 'life is beautiful'
+ console.log (quotes)
+
+ alert('Hello world');
+ alert(quotes);
+
+ let user_name = prompt ('Sheno emrin');
+ console.log (user_name);
+
+ let last_name = prompt ('Sheno mbiemrin');
+ console.log (last_name);
+
+ let user_age = prompt('Sheno moshen tuaj')
+ console.log (user_age);
+console.log('Your full name is ', user_name, last_name, 'You are', user_age, 'years')
+
+
+let mosha = 20
+if (mosha < 18) {
+    console . log ('Sorry ,you are not old enough to enter ')
+
+} else if ( mosha < 21) {
+    console.log ('welcome . You can enter') ;
+
+}
+
+
+let friends = ['Nita', 'Suela',];
+let exam_points = [78,44,23,98]
+
+let bookOne ={
+    name:'Veronika vendos te jetoj ',
+    author:'Ismail Kadare ',
+    genre:'Romance'
+}
+
+function welcome(user) {
+    alert('Hello',user)
+}
+welcome('nita');
